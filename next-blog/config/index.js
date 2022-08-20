928c201b9519b7ddc72c00c012a1164cef1ad33e
Promise Champion <promiseduke@gmail.com>
2022-08-20 23:46:39 +0100

    domain correction

--- next-blog/config/index.js
+++ next-blog/config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev
-  ? 'http://localhost:3000'
-  : 'https://nextjs-with-placeholder.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://vercel.com';
